59ab62580ef19c4569c9984bd6e4fc71f1bf8b5e
綾田陽介 <202x102x@stu.kobe-u.ac.jp>
2020-08-03 23:29:05 +0900

    implement searchTsubuyaki function

--- tsubuyaki/src/main/java/jp/kobe_u/cs/daikibo/tsubuyaki/controller/TsubuyakiController.java
+++ tsubuyaki/src/main/java/jp/kobe_u/cs/daikibo/tsubuyaki/controller/TsubuyakiController.java
@@ -1,5 +1,6 @@
 package jp.kobe_u.cs.daikibo.tsubuyaki.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import jp.kobe_u.cs.daikibo.tsubuyaki.entity.Tsubuyaki;
 import jp.kobe_u.cs.daikibo.tsubuyaki.service.TsubuyakiService;
@@ -26,6 +28,7 @@ public class TsubuyakiController {
     //メイン画面を表示
     @GetMapping("/read")
     String showTsubuyakiList(Model model) {
+        System.out.println("showTsubuyakiList");
         List<Tsubuyaki> list = ts.getAllTsubuyaki(); //全つぶやきを取得
 
         model.addAttribute("tsubuyakiList", list);   //モデル属性にリストをセット
@@ -37,6 +40,7 @@ public class TsubuyakiController {
     //つぶやきを投稿
     @PostMapping("/read")
     String postTsubuyaki(@ModelAttribute("tsubuyakiForm") TsubuyakiForm form, Model model) {
+        System.out.println("postTsubuyaki");
         //フォームからエンティティに移し替え
         Tsubuyaki t = new Tsubuyaki();
         t.setName(form.getName());
@@ -46,6 +50,19 @@ public class TsubuyakiController {
         ts.postTsubuyaki(t);
 
         return "redirect:/read"; //メイン画面に転送
+    }
+
+    // つぶやきを検索する
+    @GetMapping("/search")
+    String searchTsubuyaki(@RequestParam("keyword") String keyword, Model model) {
+        System.out.println("searchTsubuyaki");
+
+        List<Tsubuyaki> result = ts.getSearchResult(keyword);
+        model.addAttribute("keyword", keyword);
+        model.addAttribute("searchResult", result);
+        model.addAttribute("searchResultSize", result.size());
+
+        return "search";
     }
 
 }
--- tsubuyaki/src/main/java/jp/kobe_u/cs/daikibo/tsubuyaki/controller/TsubuyakiForm.java
+++ tsubuyaki/src/main/java/jp/kobe_u/cs/daikibo/tsubuyaki/controller/TsubuyakiForm.java
@@ -5,7 +5,5 @@ import lombok.Data;
 @Data
 public class TsubuyakiForm {
     String name; //投稿者
-
     String comment; //つぶやき（省略不可）
-
 }
